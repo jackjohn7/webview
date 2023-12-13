6b9756fb50bbc95f9ee7e05429b15ed2c6dea5e5
JingusJohn <branchjohnchristopher@gmail.com>
2023-12-13 13:47:08 -0600

    organized video processing imports

--- services/video_service/src/server.rs
+++ services/video_service/src/server.rs
@@ -1,17 +1,19 @@
 use tonic::{Request, Response, Status};
-use crate::video_processing::video_processing_service_server;
-use crate::video_processing::ProcessVideoRequest;
-use crate::video_processing::ProcessedVideoData;
-use crate::video_processing::DeleteVideoRequest;
-use crate::video_processing::DeleteVideoResponse;
-use crate::video_processing::HealthCheckRequest;
-use crate::video_processing::HealthCheckResponse;
+use crate::video_processing::{
+    video_processing_service_server::VideoProcessingService,
+    ProcessVideoRequest,
+    ProcessedVideoData,
+    DeleteVideoRequest,
+    DeleteVideoResponse,
+    HealthCheckRequest,
+    HealthCheckResponse
+};
 
 #[derive(Default)]
 pub struct MyVideoProcessor {}
 
 #[tonic::async_trait]
-impl video_processing_service_server::VideoProcessingService for MyVideoProcessor {
+impl VideoProcessingService for MyVideoProcessor {
     async fn process_new_video(&self, _request: Request<ProcessVideoRequest>)
         -> Result<Response<ProcessedVideoData>, Status> {
         Ok(Response::new(ProcessedVideoData { thumbnail_id: "temp".to_owned(), }))
